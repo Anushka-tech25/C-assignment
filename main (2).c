d938f437845e751c03e3c4baaaa76ce8fd25d146
Anushka-tech25 <anushkaagr10@gmail.com>
2025-04-29 17:55:51 +0530

    Update main (2).c

--- main (2).c
+++ main (2).c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-#include "header.h" 
+#include "header.h"
 
 int main() {
-    int n,a,l;
-    printf("Enter number to check even/odd :-\n");
-    scanf("%d",&l);
-    if(evenodd(l)==0){
+    int n, a, l;
+
+    printf("Enter number to check even/odd:\n");
+    scanf("%d", &l);
+    if (evenodd(&l) == 0) {
         printf("Even\n");
-    }
-    else{
+    } else {
         printf("Odd\n");
     }
 
-    printf("Enter number to find factorial :- ");
-    scanf("%d",&n);
-    printf("%d\n",factorial(n));
+    printf("Enter number to find factorial:\n");
+    scanf("%d", &n);
+    printf("Factorial: %d\n", factorial(&n));
 
-    printf("Enter number to check prime :- ");
-    scanf("%d",&a);
-    if (prime(a)==1){
-        printf("prime");
-    }
-    else{
-        printf("Not");
+    printf("Enter number to check prime:\n");
+    scanf("%d", &a);
+    if (prime(&a) == 1) {
+        printf("Prime\n");
+    } else {
+        printf("Not Prime\n");
     }
 
+    return 0;
 }
+
+   
