b068439bc0de069e56e9db7481e6158270feba5c
Anushka-tech25 <anushkaagr10@gmail.com>
2025-04-29 17:54:17 +0530

    Update header.h

--- header.h
+++ header.h
@@ -1,40 +1,33 @@
+#ifndef HEADER_H
+#define HEADER_H
 
+int factorial(int *n);
+int evenodd(int *l);
+int prime(int *a);
 
-int factorial(int n);
-int evenodd(int l);
-int prime(int a);
-int evenodd(int l){
-    if(l%2==0){
-        return 0;
+int evenodd(int *l) {
+    return (*l % 2 == 0) ? 0 : 1;
+}
+
+int prime(int *a) {
+    if (*a <= 1) return 0;
+    for (int i = 2; i <= *a / 2; i++) {
+        if (*a % i == 0) return 0;
     }
     return 1;
 }
 
-int prime(int a){
-    int b=1;
-    if(a==1 || a==0){
-        b=0;
-    }
-    for(int i =2;i<a/2;i++){
-        if(a%i==0){
-            b=0;
-        }
-    }     
-    if(b==1){
-        return 1;
-
-    }
-    else{
-        return 0;
-    }
-}
-
-int factorial(int n){
-    int prod=1;
-    for (int i =1;i<=n;i++){
-        prod*=i;
+int factorial(int *n) {
+    int prod = 1;
+    for (int i = 1; i <= *n; i++) {
+        prod *= i;
     }
     return prod;
+}
 
-}  
+#endif
+
+
+
+    
 
